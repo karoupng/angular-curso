0842665148bf6c3eb8da171727b54ae1bb91512f
Karolaine Pereira <karolainepereira2012@hotmail.com>
2026-06-01 20:26:00 -0300

    atualizando

--- Parte 1 - Instalando o Angular versão -no standalone-/estudo-modulos/src/app/app.component.spec.ts
+++ Parte 1 - Instalando o Angular versão -no standalone-/estudo-modulos/src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-16-no-standalone'`, () => {
+  it(`should have as title 'estudo-modulos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-16-no-standalone');
+    expect(app.title).toEqual('estudo-modulos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-16-no-standalone app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('estudo-modulos app is running!');
   });
 });
