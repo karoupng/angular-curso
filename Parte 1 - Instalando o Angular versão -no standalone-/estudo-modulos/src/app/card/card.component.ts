87294d2b03da09402e6b52f2344068667202de07
Karolaine Pereira <karolainepereira2012@hotmail.com>
2026-06-05 11:07:35 -0300

    binding

--- Parte 1 - Instalando o Angular versão -no standalone-/estudo-modulos/src/app/card/card.component.ts
+++ Parte 1 - Instalando o Angular versão -no standalone-/estudo-modulos/src/app/card/card.component.ts
@@ -3,16 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  tipo = 'Simples';
-  preco = 100.00;
-
-  
-getTitle(){
-  return 'Plano' + ' ' + this.tipo;
-
+  // tipo = 'Simples';
+  // preco = 100.00;
+  plano = {
+    tipo: 'Simples',
+    preco: 200
+  };
 }
 
-}
+// getTitle(){
+//   return 'Plano' + ' ' + this.tipo;
